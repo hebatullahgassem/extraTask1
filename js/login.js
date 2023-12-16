043e5fb5a1207ea1196688ed14e36d2f697c3dd3
hebatullah <hebatullah@gassem>
2023-12-16 07:00:28 +0200

    loginPage-localstorage

--- js/login.js
+++ js/login.js
@@ -5,11 +5,18 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
+  //localstorage
+  localStorage.setItem('userEmail', email);
+  localStorage.setItem('userPassword', password);
+
+  var getEmail = localStorage.getItem('userEmail');
+  var getPassword = localStorage.getItem('userPassword');
+
   //load the json file
   fetch('../json/login.json').then(response => response.json())
   .then(data => {
     //check if user exist and password correct
-    var user = data.find(user => user.email === email && user.password === password);
+    var user = data.find(user => user.email === getEmail && user.password === getPassword);
 
     if(user){
       window.location.href = "../pages/home.html";
